5a8c0df856dc3bab9191d6b02a5750668b28921c
support-magsway <viswanath608@gmail.com>
2024-05-11 20:40:25 -0700

    Fixed methods

--- Backend/index.py
+++ Backend/index.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/api/v1/prompt", method=['POST'])
+@app.route("/api/v1/prompt", methods=['POST'])
 def promt():
     if request.method == 'POST':
         PROMPT = 'You as a assistant to Andrew Huberman, pick the vitamins or supplements that he suggests for the user asked input of'+request.form['promptdata']+'JUST MENTION THE SUPPLEMENT OR PROTEIN NOTHING ELSE.'
