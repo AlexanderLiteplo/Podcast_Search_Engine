5d4dc103b3cec78022fbdd34737ada33a0e55647
support-magsway <viswanath608@gmail.com>
2024-05-11 18:04:07 -0700

    Frontend and Backend code

---
+++ Backend/graphlit.py
@@ -0,0 +1,9 @@
+organization_id = "ccfd3f42-44cd-4964-b3af-87b100a539a8"
+environment_id = "e6f7f802-9278-4ce4-8d4d-1680fe0db620"
+jwt_secret= "CA9qBlX2I8PXeWAHGbNF+iCBwq9F+fEyr4s4YrrSEko="
+
+graphlit = Graphlit(
+    organization_id=organization_id, 
+    environment_id=environment_id, 
+    jwt_secret=jwt_secret
+)
